225a3d9fdb9717052d5db6043a46e455dec06f1c
matias farah <maticole1980@gmail.com>
2023-10-05 16:40:27 -0300

    primer entrega React

---
+++ EcommerceEP/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ItemListContainer from './Components/itemListContainer/itemListContainer'
+import NavBar from './Components/navBar/navBar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <ItemListContainer greeting= "Hola, Gracias por ingresar a (EP)"/>
+    </>
+  )
+}
+
+export default App
